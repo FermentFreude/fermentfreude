a398ca92223442334425e4ba86958bbe64afa44b
alaashaheen <alaa.shaheen@students.beaminstitute.org>
2026-02-18 10:08:47 +0100

    Update payload types

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -337,7 +337,6 @@ export interface Media {
     };
     [k: string]: unknown;
   } | null;
-  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -1799,7 +1798,6 @@ export interface CategoriesSelect<T extends boolean = true> {
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
   caption?: T;
-  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
