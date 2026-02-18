519a8c443d4632d7e4a39e67020465b72f50ef39
Rafaela <rafaela.vaz.quaresma@students.beaminstitute.org>
2026-02-18 16:51:06 +0100

    chore: regenerate payload types (add prefix field to Media)

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -337,6 +337,7 @@ export interface Media {
     };
     [k: string]: unknown;
   } | null;
+  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -1961,6 +1962,7 @@ export interface CategoriesSelect<T extends boolean = true> {
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
   caption?: T;
+  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
